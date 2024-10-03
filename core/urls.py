529d31ed36d9f4e53da4840832f9b6e09bbc1cda
marse11e <endelss.key.2018@gmail.com>
2024-10-03 05:10:02 +0500

    add swagger and redoc 031024 5-10

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API для учета расходов",
+        default_version='v1',
+        description="Документация к API для отслеживания личных финансов.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(name="Marselle Naz", url="https://t.me/MarselleNaz",email="marselle.naz@yandex.kz"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -7,4 +23,6 @@ urlpatterns = [
     path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('apps.main.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
